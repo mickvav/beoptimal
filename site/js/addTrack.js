fc9efd3502a3e46dbdc060e5de9b23e3bb7016fc
Mihail Vasilev <mickvav@gmail.com>
2017-08-06 01:02:51 +0300

    Some gps stuff works.

--- site/js/addTrack.js
+++ site/js/addTrack.js
@@ -3,7 +3,39 @@ function addTrack() {
    $(".buttons").addClass("hidden");
 
    $("#overlaybox-gps").removeClass("hidden");
-   $("#overlaybox-gps").addClass("visible");
-
-   
+   $("#overlaybox-gps").addClass("visible"); 
 };
+
+function closeTrack() {
+   $(".buttons").addClass("visible");
+   $(".buttons").removeClass("hidden");
+
+   $("#overlaybox-gps").addClass("hidden");
+   $("#overlaybox-gps").removeClass("visible"); 
+};
+
+function GPShandleFileSelect(evt) {
+   var files = evt.target.files; // FileList
+   for (var i = 0, f; f = files[i]; i++) {
+            
+       var reader = new FileReader();
+       reader.onload = (function(theFile) {
+         return function(e) {
+           var xml=jQuery.parseXML(e.target.result);
+           var parser = new GPXParser(xml, map);
+           parser.setTrackColour("#ff0000");
+           parser.setTrackWidth(5);
+           parser.setMinTrackPointDelta(0.005);
+           parser.centerAndZoom(xml);
+           parser.addTrackpointsToMap();  
+           parser.addRoutepointsToMap();
+           parser.addWaypointsToMap(); 
+           closeTrack();
+         };
+       })(f);
+       reader.readAsText(f);
+   };
+}
+ 
+
+
